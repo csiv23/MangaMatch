602b74f64e47efc4fcf68c0c854f8962f9396ccd
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-07 14:48:31 -0400

    Fixed frontend/backend communication

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const searchRouter = require('./routes/search');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
