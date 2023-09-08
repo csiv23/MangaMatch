743b3ca64f692c93e4343dea40fa3508ed2ce741
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-07 20:27:39 -0400

    Filtered recommendations results to remove multiple from same series

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Manga = require('../models/Manga');
@@ -16,19 +17,55 @@ async function processBatch(targetVector, batch) {
 
 router.post('/', async (req, res) => {
     try {
-        const { mangaIds } = req.body;
+        const mangaIds = req.body.mangaIds.map(id => Number(id));
         const targetMangas = await Manga.find({ manga_id: { $in: mangaIds } });
+
+        // Create a set of lowercase titles for the target mangas to facilitate the case-insensitive comparison
+        const targetTitles = new Set(targetMangas.map(manga => manga.title.toLowerCase()));
         const allMangas = await Manga.find({});
 
         const targetVectors = targetMangas.map(mangaToVector);
         const allVectors = allMangas.map(manga => ({ item: manga, vector: mangaToVector(manga) }));
         const avgVector = computeAverageVector(targetVectors);
 
-        const filteredVectors = allVectors.filter(vec => !mangaIds.includes(vec.item.manga_id.toString()));
+        // Filter out mangas that have a title containing a part of any of the target manga titles
+        const filteredVectors = allVectors.filter(vec => {
+            const lowerCaseVecTitle = vec.item.title.toLowerCase();
+            return !mangaIds.includes(vec.item.manga_id) &&
+                ![...targetTitles].some(title => {
+                    const titleWords = title.split(' ');
+                    return titleWords.includes(lowerCaseVecTitle) ||
+                        title === lowerCaseVecTitle ||
+                        titleWords.some(word => lowerCaseVecTitle.includes(word));
+                });
+        });
+
+        // Identifying the base title to categorize manga into series
+        const getBaseTitle = (title) => {
+            const separators = [':', '-'];
+            let baseTitle = title;
+            separators.forEach((sep) => {
+                if (title.includes(sep)) {
+                    baseTitle = title.split(sep)[0].trim();
+                }
+            });
+            return baseTitle.toLowerCase();
+        };
+
+        // Further filtering vectors to include only one manga per series
+        const uniqueSeriesVectors = [];
+        const uniqueSeriesTitles = new Set();
+        filteredVectors.forEach(vec => {
+            const baseTitle = getBaseTitle(vec.item.title);
+            if (!uniqueSeriesTitles.has(baseTitle)) {
+                uniqueSeriesVectors.push(vec);
+                uniqueSeriesTitles.add(baseTitle);
+            }
+        });
 
         let topMangas = [];
-        for (let i = 0; i < filteredVectors.length; i += OPTIMAL_BATCH_SIZE) {
-            const batch = filteredVectors.slice(i, i + OPTIMAL_BATCH_SIZE);
+        for (let i = 0; i < uniqueSeriesVectors.length; i += OPTIMAL_BATCH_SIZE) {
+            const batch = uniqueSeriesVectors.slice(i, i + OPTIMAL_BATCH_SIZE);
             const batchResult = await processBatch(avgVector, batch);
             topMangas = topMangas.concat(batchResult);
         }
