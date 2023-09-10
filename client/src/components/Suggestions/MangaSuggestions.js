32e3c53ab562a26e1a7d1ab4c6290ab2b9281134
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-09 20:20:05 -0400

    Added images to suggestions bar

--- client/src/components/Suggestions/MangaSuggestions.js
+++ client/src/components/Suggestions/MangaSuggestions.js
@@ -7,14 +7,15 @@ function MangaSuggestions({ suggestions, handleSelectManga }) {
                 {suggestions.map((suggestion) => (
                     <button
                         type="button"
-                        className="list-group-item list-group-item-action"
+                        className="list-group-item list-group-item-action d-flex align-items-center"
                         key={suggestion.manga_id}
                         onClick={() => handleSelectManga(suggestion)}
                     >
+                        {/* Display manga main picture next to the suggestion */}
+                        <img src={suggestion.main_picture} alt={`${suggestion.title} cover`} style={{ width: '50px', marginRight: '10px' }} />
                         {suggestion.title}{suggestion.info ? ` - ${suggestion.info}` : ''}
                     </button>
                 ))}
-
             </div>
         )
     );
