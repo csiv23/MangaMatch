248f10e090aed09517a13e67d44fcd6bee31df33
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-18 22:46:14 -0400

    Added further styling

--- frontend/src/components/Suggestions/MangaSuggestions.js
+++ frontend/src/components/Suggestions/MangaSuggestions.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import './MangaSuggestions.css'; // Importing a new CSS file to style this component
 
 function MangaSuggestions({ suggestions, handleSelectManga }) {
     return (
         suggestions.length > 0 && (
-            <div className="list-group">
+            <div className="list-group manga-suggestions-container">
                 {suggestions.map((suggestion) => (
                     <button
                         type="button"
-                        className="list-group-item list-group-item-action d-flex align-items-center"
+                        className="list-group-item list-group-item-action d-flex align-items-center suggestion-item" // Added a new class for styling
                         key={suggestion.manga_id}
                         onClick={() => handleSelectManga(suggestion)}
                     >
-                        {/* Display manga main picture next to the suggestion */}
-                        <img src={suggestion.main_picture} alt={`${suggestion.title} cover`} style={{ width: '50px', marginRight: '10px' }} />
-                        {suggestion.title}{suggestion.info ? ` - ${suggestion.info}` : ''}
+                        <img src={suggestion.main_picture} alt={`${suggestion.title} cover`} className="suggestion-img" /> {/* Added a new class for styling */}
+                        <div className="suggestion-text"> {/* Wrapped text in a div to apply styles */}
+                            {suggestion.title}{suggestion.info ? ` - ${suggestion.info}` : ''}
+                        </div>
                     </button>
                 ))}
             </div>
