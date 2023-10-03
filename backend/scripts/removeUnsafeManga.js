e794f3314f6e443b6716a34a18a7626c538182e1
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-10-03 09:41:40 -0400

    Create removeUnsafeManga.js

---
+++ backend/scripts/removeUnsafeManga.js
@@ -0,0 +1,31 @@
+// removeUnsafeManga.js
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = 'mongodb://localhost:27017';
+const dbName = 'MangaMatch';
+const collectionName = 'Manga2';
+
+(async () => {
+  // Create a new MongoClient
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+    
+    // Get a handle on the database and collection
+    const collection = client.db(dbName).collection(collectionName);
+
+    // Remove documents where sfw is false
+    const deleteResult = await collection.deleteMany({ sfw: false });
+    
+    // Log the number of removed documents
+    console.log(`Successfully deleted ${deleteResult.deletedCount} entries where sfw is false.`);
+  } catch (err) {
+    // Log any errors that occur
+    console.error(`Failed to delete entries: ${err.message}`);
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+})();
