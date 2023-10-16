f8133c0bc9bf11f2a34cc48207ee169acc955e33
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-10-16 12:53:51 -0400

    Added more information to response display and included read more button

--- frontend/src/components/ResponseDisplay/ResponseDisplay.js
+++ frontend/src/components/ResponseDisplay/ResponseDisplay.js
@@ -10,13 +10,11 @@ function ResponseDisplay() {
 
     const location = useLocation();
 
-
     const handleBackToSearch = () => {
         setSelectedManga([]);  // Reset the selected manga to an empty array
         setSelectedMangaIds([]);  // Reset the selected manga IDs to an empty array
         location.push('/');
     };
-
 
     return (
         <div className="container mt-3">
@@ -25,50 +23,21 @@ function ResponseDisplay() {
             </Link>
             <div className="d-flex flex-wrap justify-content-around">
                 {responseData.map((manga, index) => {
-                    console.log(`Genres type: ${typeof manga.genres}`, manga.genres);
-
-                    // Parse the genres string to get an array and join it to get a string
-                    let genresList = 'Undefined';
-                    let genresLabel = 'Genres';
-                    if (manga.genres && manga.genres.length > 0) {
-                        try {
-                            const genresArray = JSON.parse(manga.genres[0].replace(/'/g, '"'));
-                            genresList = genresArray.join(', ');
-                            genresLabel = genresArray.length === 1 ? 'Genre' : 'Genres';
-                        } catch (error) {
-                            console.error('Error processing genres:', error);
-                        }
-                    }
-                    // Process authors to get a string representation and determine the appropriate label
-                    let authorsList = 'Undefined';
-                    let authorsLabel = 'Authors';
-                    if (manga.authors && manga.authors.length > 0) {
-                        try {
-                            authorsList = manga.authors
-                                .map(authorString => {
-                                    const authorArray = JSON.parse(authorString.replace(/'/g, '"'));
-                                    return authorArray
-                                        .map(author => `${author.first_name} ${author.last_name}`.trim())
-                                        .join(', ');
-                                })
-                                .join(', ');
-
-                            authorsLabel = manga.authors.length === 1 ? 'Author' : 'Authors';
-                        } catch (error) {
-                            console.error('Error processing authors:', error);
-                        }
-                    }
-
                     return (
                         <div className="card m-2" style={{ width: '18rem' }} key={index}>
                             <img src={manga.main_picture} alt={`${manga.title} cover`} className="card-img-top manga-image" />
                             <div className="card-body manga-text">
-                                <h5 className="card-title">{manga.title}</h5>
+                                <h5 className="card-title">
+                                    <a href={manga.url} target="_blank" rel="noopener noreferrer">{manga.title}</a>
+                                </h5>
                                 <h6 className="card-subtitle mb-2 text-muted">{manga.type} - Score: {manga.score}</h6>
                                 <p className="card-text">
-                                    {genresLabel}: {genresList} <br />
-                                    {authorsLabel}: {authorsList}
+                                    {manga.synopsis}
                                 </p>
+                                {/* Add a link to MyAnimeList */}
+                                <a href={manga.url} target="_blank" rel="noopener noreferrer" className="mal-link">
+                                    Read more on MyAnimeList
+                                </a>
                             </div>
                         </div>
                     );
@@ -79,5 +48,3 @@ function ResponseDisplay() {
 }
 
 export default ResponseDisplay;
-
-
